75eba1e36e7ed28bcf19623ac0556484c7d38fa3
Ivan Ruchkin <rururuchkin@gmail.com>
2024-12-22 00:24:07 +0300

    alpha build

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_ngrok import run_with_ngrok
 from flask_migrate import Migrate
+
 from dotenv import load_dotenv
 import logging, base64, os
 
@@ -14,14 +14,11 @@ logging.basicConfig(level=logging.INFO,
                     ])
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
-
 load_dotenv()
 
 app = Flask(__name__)
-# app.secret_key = os.getenv("TOKEN")
-# run_with_ngrok(app)
 
-app.secret_key = base64.b64encode('SUPER_KEY'.encode()).decode()
+app.secret_key = base64.b64encode(str(os.getenv("KEY")).encode()).decode()
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auth.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -40,8 +37,3 @@ from src import models, main_routers, buy_routers, admin_routers
 def create_tables():
     app.before_request_funcs[None].remove(create_tables)
     db.create_all()
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
--- starter.py
+++ starter.py
@@ -1,4 +1,11 @@
+from threading import Thread
 from src import app
+import os
+
+def run_tunnel():
+    result = os.system('tuna http 8080')
 
 if __name__ == "__main__":
+    start = Thread(target=run_tunnel)
+    #start.start()
     app.run(use_reloader=True, port=8080)
